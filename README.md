49deb375e542b64140429919b2756386747effbe
Alex Sartori <alex.sartori1997@gmail.com>
2023-02-02 12:10:33 +0100

    Use parsed timastamps as indexes

--- README.md
+++ README.md
@@ -27,35 +27,34 @@ data = LogSession('2022_09_26_Vadena')
 Track sessions are the different telemetry sessions
 
 ```python
-print(data.track.sessions)
+print(data.track_sessions)
 
 # [Straight line [2-step throttle + hard brake] 004, Straight line [2-step throttle + hard brake] 006, ...]
 ```
 
-Each track session has a `logs` array of Pandas DataFrames for each CSV
+Each track session has a `name` string and a `logs` array of Pandas DataFrames, one for each CSV
 
 ```python
-print(l.track_sessions[0].logs['HV_CELLS_TEMP'])
+print(l.track_sessions[0].logs['STEERING_ANGLE'])
 
-#            _timestamp  start_index     temp_0     temp_1     temp_2     temp_3     temp_4     temp_5
-# 0    1664196134689841          198  28.235294  27.843138  28.627451  28.235294  28.627451  28.235294
-# 1    1664196134739434          204  27.843138  27.843138  27.058823  27.843138  28.235294  28.235294
-# 2    1664196134789407          210  26.666666  27.058823  27.058823  27.450981  27.450981  27.450981
-# 3    1664196134839336            0  28.235294  27.843138  27.843138  27.450981  27.450981  27.058823
-# 4    1664196134889344            6  28.235294  28.627451  27.843138  28.235294  28.627451  28.235294
-# ..                ...          ...        ...        ...        ...        ...        ...        ...
-# 139  1664196141639534          168  27.843138 -20.000000 -20.000000  27.843138  27.843138  27.843138
-# 140  1664196141689538          174 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000
-# 141  1664196141739542          180 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000
-# 142  1664196141840020          192 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000 -20.000000
-# 143  1664196141889891          198  28.235294  27.843138  28.627451  28.235294  28.627451  28.235294
-# 
-# [144 rows x 8 columns]
+#                                 angle
+# _timestamp
+# 2022-12-12 13:10:17.887097  21.710159
+# 2022-12-12 13:10:17.897410  21.710159
+# 2022-12-12 13:10:17.907743  21.710159
+# 2022-12-12 13:10:17.918736  21.710159
+# 2022-12-12 13:10:17.928678  21.710159
+# ...                               ...
+# 2022-12-12 13:11:51.148411   7.647659
+# 2022-12-12 13:11:51.159656   7.647659
+# 2022-12-12 13:11:51.170396   7.647659
+# 2022-12-12 13:11:51.181272   7.647659
+# 2022-12-12 13:11:51.192226   7.647659
 ```
 
 ## Features
 
 - [x] Allow to load only a selected set of logs and not the entire folder tree
-- [ ] Use timestamps as dataframe indexes, parsed as a DateTimeIndex instead of Int64Index
+- [x] Use timestamps as dataframe indexes, parsed as a DateTimeIndex instead of Int64Index
 - [ ] Resample dataframes at a specified frequency
 - [ ] Align dataframes if first/last timestamps do not match
--- pytlm/LogReader.py
+++ pytlm/LogReader.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 from typing import Iterator, List
+from itertools import chain
 import pandas as pd
 from pathlib import Path
 
 
 class LogSession:
     def __init__(self, path: str, subset: List[str] = None) -> None:
+        self.options = {
+
+        }
+
         p = Path(path)
 
         if p.is_dir():
@@ -14,7 +19,7 @@ class LogSession:
         else:
             raise FileNotFoundError("Not a directory: %s" % path)
         
-        print("Loading log session from disk: %s" % self.name)
+        print("[pytlm] Loading log session from disk: %s" % self.name)
         self.track_sessions = list(self.__get_track_sessions(subset))
 
     def __get_track_sessions(self, subset: List[str] = None) -> Iterator[TrackSession]:
@@ -40,27 +45,30 @@ class TrackSession:
         else:
             raise FileNotFoundError("Not a directory: %s" % path)
 
-        print("    Loading track session from disk: %s" % self.name)
+        print("[pytlm]    Loading track session from disk: %s" % self.name)
         self.logs = self.__get_logs_data()
 
     def __get_logs_data(self) -> dict[str, Iterator[pd.DataFrame]]:
+
+        def csv_to_df(fname):
+            df = pd.read_csv(csv_log)
+            df['_timestamp'] = pd.to_datetime(df['_timestamp'], unit='us')
+            df = df.set_index('_timestamp')
+            return df
+
         result = {}
         folder_pri = self.path / 'Parsed' / 'primary'
         folder_sec = self.path / 'Parsed' / 'secondary'
         folder_gps = self.path / 'Parsed' / 'GPS'
+        ignore_files = ['GPS_GGA.csv']
 
         # Note: if files are empty, Pandas will throw an error. For some reason,
         # empty files are larger than 0 bytes, as they probably contain a newline.
 
-        for csv_log in folder_pri.iterdir():
-            if csv_log.stat().st_size > 2:
-                result[csv_log.stem] = pd.read_csv(csv_log)
-        for csv_log in folder_sec.iterdir():
-            if csv_log.stat().st_size > 2:
-                result[csv_log.stem] = pd.read_csv(csv_log)
-        for csv_log in folder_gps.iterdir():
-            if csv_log.stat().st_size > 2 and csv_log.name != "GPS_GGA.csv":
-                result[csv_log.stem] = pd.read_csv(csv_log)
+        for csv_log in chain(folder_pri.iterdir(), folder_sec.iterdir(), folder_gps.iterdir()):
+            if csv_log.stat().st_size > 2 and csv_log.name not in ignore_files:
+                result[csv_log.stem] = csv_to_df(csv_log)
+
         return result
 
     def __repr__(self) -> str:
@@ -68,3 +76,9 @@ class TrackSession:
 
     def __str__(self) -> str:
         return self.name
+
+
+if __name__ == '__main__':
+    logs = LogSession('../../CorneringSpeed/2022_12_12_Vadena', subset=['Half Skidpad CW [#1] 000']) # , 'Warmup m40 C0 [#1] 003'])
+    resample_resolution_us = 1000
+    print(logs.track_sessions[0].logs['STEERING_ANGLE'])
